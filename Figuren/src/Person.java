9d139eff19099096e6875894390c0dbc769e6331
Theresa Buchner <thb9494@thi.de>
2024-06-14 13:43:25 +0200

    Person funktioniert

--- Figuren/src/Person.java
+++ Figuren/src/Person.java
@@ -28,9 +28,19 @@ public class Person extends Figur{
     
     // Zeichnen der Person
     //head 
-    g.fillOval(offsetLeft + armsWidth * factor + (mainBodyWidth/ 2) * factor, offSetTop, headRadius * factor, headRadius * factor);
+    g.fillOval(offsetLeft + armsWidth * factor + (neckWidth/2) * factor, offSetTop, headRadius * factor, headRadius * factor);
     //neck
-    g.fillRect(offsetLeft + armsWidth * factor + (mainBodyWidth/2) * factor, offSetTop + (int) (headRadius * factor * 0.75), neckWidth * factor, neckHeight * factor);
+    g.fillRect(offsetLeft + armsWidth * factor + (mainBodyWidth/3) * factor, offSetTop + (int) (headRadius * factor * 0.75), neckWidth * factor, neckHeight * factor);
+    //body
+    g.fillRect(offsetLeft + armsWidth * factor, offSetTop + (int) (headRadius * 0.75 * factor) + neckHeight * factor, mainBodyWidth * factor, mainBodyHeight * factor);
+    //arm 1
+    g.fillRect(offsetLeft, offSetTop + (int) (headRadius * 0.75 * factor) + neckHeight * factor, armsWidth * factor, armsHeight * factor);
+    //arm 2
+    g.fillRect(offsetLeft + armsWidth * factor + mainBodyWidth * factor, offSetTop + (int) (headRadius * 0.75 * factor) + neckHeight * factor, armsWidth * factor, armsHeight * factor);
+    //leg 1
+    g.fillRect(offsetLeft + armsWidth * factor , offSetTop + (int) (headRadius * 0.75 * factor) + neckHeight * factor + mainBodyHeight * factor, legsWidth * factor, legsHeight * factor);
+    //leg 2
+    g.fillRect(offsetLeft + armsWidth * factor + (mainBodyWidth - legsWidth) * factor, offSetTop + (int) (headRadius * 0.75 * factor) + neckHeight * factor + mainBodyHeight * factor, legsWidth * factor, legsHeight * factor);
 
   }
   
